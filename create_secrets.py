caacb5c459de8f5e56f0b7d2e9a2359a14f1b5f2
Tom Noble <tom@rivelinrobotics.com>
2024-06-10 08:16:50 +0100

    Replaces repository with repositories

--- create_secrets.py
+++ create_secrets.py
@@ -47,6 +47,7 @@ def create_secret(base_url: str, token: str, public_key_id: str, secret_name: st
 
 if __name__ == "__main__":
     repositories, environment, token, secrets = sys.argv[1:]
+    print(repositories)
     base_url = "https://api.github.com"
     for repository in repositories.split(" "):
         environment_url = f"{base_url}/repos/{repository}/environments/{environment}"
